b37fe93c23a97140d7baa305eedf9a2ffd939ba3
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-10-14 21:24:09 +0200

    updated main and v2

--- maxsum/maxsum_v2.c
+++ maxsum/maxsum_v2.c
@@ -12,13 +12,17 @@ double maxsum(int N, int LD, double *A, int NT) {
   int end_row = 0;
   int i = 0, j = 0;
 
+  omp_lock_t sem;
+  omp_init_lock(&sem);
+
   omp_set_num_threads(NT);
   #pragma omp parallel firstprivate(thread_max_sum_of_roots, sum_of_roots_row_i, \
-                                      thread_id, thread_N, start_row, end_row, i, j)
+                                      thread_id, thread_N, start_row, end_row, \
+                                      i, j)
   {
     thread_id = omp_get_thread_num();
     start_row = thread_N * thread_id;
-    end_row = (thread_id == NT - 1 ? N : start_row + thread_N);
+    end_row = thread_N * (thread_id + 1);
 
     for (i = start_row; i < end_row; i++) {
       sum_of_roots_row_i = 0;
@@ -30,12 +34,11 @@ double maxsum(int N, int LD, double *A, int NT) {
         thread_max_sum_of_roots = sum_of_roots_row_i;
       }
     }
-    #pragma omp critical
-    {
-      if (max_sum_of_roots < thread_max_sum_of_roots) {
-        max_sum_of_roots = thread_max_sum_of_roots;
-      }
+    omp_set_lock(&sem);
+    if (max_sum_of_roots < thread_max_sum_of_roots) {
+      max_sum_of_roots = thread_max_sum_of_roots;
     }
+    omp_unset_lock(&sem);
   }
 
   return max_sum_of_roots;
