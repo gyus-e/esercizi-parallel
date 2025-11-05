0952a66408bbd15a808c49f9ba22876049a7951b
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-11-05 12:04:44 +0100

    laplace blocking

---
+++ laplace/laplace_blocking.c
@@ -0,0 +1,95 @@
+#include <mpi.h>
+
+float get_B_ij(float up, float down, float left, float right) {
+  return (up + down + left + right) * 0.25;
+}
+
+void init_first_row(float *A, float *B, float *daprev, int N, int LD) {
+  int j;
+  float up, down, left, right;
+
+  for (j = 1; j < N - 1; j++) {
+    up = daprev[j];
+    down = A[1 * LD + j];
+    left = A[0 * LD + (j - 1)];
+    right = A[0 * LD + (j + 1)];
+
+    B[0 * LD + j] = get_B_ij(up, down, left, right);
+  }
+}
+
+void init_row_i(int i, float *A, float *B, int N, int LD) {
+  int j;
+  float up, down, left, right;
+
+  for (j = 1; j < N - 1; j++) {
+    up = A[(i - 1) * LD + j];
+    down = A[(i + 1) * LD + j];
+    left = A[i * LD + (j - 1)];
+    right = A[i * LD + (j + 1)];
+
+    B[i * LD + j] = get_B_ij(up, down, left, right);
+  }
+}
+
+void init_last_row(float *A, float *B, float *danext, int rows_per_proc, int N,
+                   int LD) {
+  int j;
+  float up, down, left, right;
+
+  for (j = 1; j < N - 1; j++) {
+    up = A[(N - 2) * LD + j];
+    down = danext[j];
+    left = A[(rows_per_proc - 1) * LD + (j - 1)];
+    right = A[(rows_per_proc - 1) * LD + (j + 1)];
+
+    B[(rows_per_proc - 1) * LD + j] = get_B_ij(up, down, left, right);
+  }
+}
+
+void copy_rows(int start_row, int end_row, float *A, float *B, int N, int LD) {
+  int i, j;
+  for (i = start_row; i < end_row; i++) {
+    for (j = 1; j < N - 1; j++) {
+      A[i * LD + j] = B[i * LD + j];
+    }
+  }
+}
+
+void laplace(float *A, float *B, float *daprev, float *danext, int N, int LD,
+             int Niter) {
+  int i, j, k;
+  int nproc, myid;
+  int rows_per_proc, start_row, end_row;
+  MPI_Status status;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+  rows_per_proc = N / nproc;
+  start_row = 1;
+  end_row = rows_per_proc - 2;
+
+  for (k = 0; k < Niter; k++) {
+    if (myid > 0) {
+      MPI_Send(&A[0], N, MPI_FLOAT, myid - 1, 0, MPI_COMM_WORLD);
+      MPI_Recv(daprev, N, MPI_FLOAT, myid - 1, 0, MPI_COMM_WORLD, &status);
+      init_first_row(A, B, daprev, N, LD);
+      start_row = 0;
+    }
+
+    for (i = 1; i < rows_per_proc - 1; i++) {
+      init_row_i(i, A, B, N, LD);
+    }
+
+    if (myid < nproc - 1) {
+      MPI_Send(&A[rows_per_proc - 1], N, MPI_FLOAT, myid + 1, 0,
+               MPI_COMM_WORLD);
+      MPI_Recv(danext, N, MPI_FLOAT, myid + 1, 0, MPI_COMM_WORLD, &status);
+      init_last_row(A, B, danext, rows_per_proc, N, LD);
+      end_row = rows_per_proc - 1;
+    }
+
+    copy_rows(start_row, end_row, A, B, N, LD);
+  }
+}
--- laplace/mainlaplace.c
+++ laplace/mainlaplace.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main (int argc, char *argv[ ]) {
+int main(int argc, char *argv[]) {
   int nproc, myid, prev, next;
   int N, i, j, ifirst, iter, Niter, LD;
   float *A, *Anew, *daprev, *danext;
   MPI_Status status;
   double get_cur_time(), t1, t2;
-  void laplace (float *, float *, float *, float *, int, int, int);
+  void laplace(float *, float *, float *, float *, int, int, int);
 
   MPI_Init(&argc, &argv);
 
@@ -17,55 +19,61 @@ int main (int argc, char *argv[ ]) {
   printf("hello from %d di %d processi \n", myid, nproc);
   sleep(1);
 
-
   N = 400;
   Niter = 8000;
   LD = 500;
-  A = (float*)malloc(500*500*sizeof(float));
-  Anew = (float*)malloc(500*500*sizeof(float));
-  daprev = (float*)malloc(500*sizeof(float));
-  danext = (float*)malloc(500*sizeof(float));
+  A = (float *)malloc(500 * 500 * sizeof(float));
+  Anew = (float *)malloc(500 * 500 * sizeof(float));
+  daprev = (float *)malloc(500 * sizeof(float));
+  danext = (float *)malloc(500 * sizeof(float));
 
   // inizializzazione matrice
 
-  for (i = 0; i< N/nproc; i++){              // tutta la matrice locale = 0
-  for (j = 0; j< N; j++){
-          A[i*LD+j] = 0.;
-  }}
+  for (i = 0; i < N / nproc; i++) { // tutta la matrice locale = 0
+    for (j = 0; j < N; j++) {
+      A[i * LD + j] = 0.;
+    }
+  }
   if (myid == 0)
-         for (j = 0; j < N; j++)
-                 A[0*LD+j] = j;                // prima riga matrice del proc id=0  da 0 a 390
+    for (j = 0; j < N; j++)
+      A[0 * LD + j] = j; // prima riga matrice del proc id=0  da 0 a 390
 
-  if (myid == nproc-1)
-         for (j = 0; j < N; j++)
-                 A[ (N/nproc-1)*LD+j] = N - 1 - j;      // ultima riga matrice del proc id=nproc-1 da 390 a 0
+  if (myid == nproc - 1)
+    for (j = 0; j < N; j++)
+      A[(N / nproc - 1) * LD + j] =
+          N - 1 - j; // ultima riga matrice del proc id=nproc-1 da 390 a 0
 
-  ifirst = myid * N/nproc;
-  for (i = 0; i < N/nproc; i++){
-          A[i*LD+0] = ifirst + i;             // bordo sinistro da ifirst a ilast-1 in ogni proc
-          A[i*LD+N-1] = N - 1 - A[i*LD+0];      // A[i][0] + A[i][N-1] = 0 sempre
+  ifirst = myid * N / nproc;
+  for (i = 0; i < N / nproc; i++) {
+    A[i * LD + 0] =
+        ifirst + i; // bordo sinistro da ifirst a ilast-1 in ogni proc
+    A[i * LD + N - 1] = N - 1 - A[i * LD + 0]; // A[i][0] + A[i][N-1] = 0 sempre
   }
 
-
-  if (myid ==0) printf("\n esecuzione con N = %d  e %d iterazioni\n\n", N, Niter);
+  if (myid == 0)
+    printf("\n esecuzione con N = %d  e %d iterazioni\n\n", N, Niter);
 
   t1 = get_cur_time();
 
-
- laplace (A, Anew, daprev, danext, N, LD, Niter);
-
-
+  laplace(A, Anew, daprev, danext, N, LD, Niter);
 
   t2 = get_cur_time();
 
-  if (myid == 0) printf("con %d processi, il tempo e' %f\n", nproc, t2-t1);
+  if (myid == 0)
+    printf("con %d processi, il tempo e' %f\n", nproc, t2 - t1);
 
   sleep(1);
-  if (myid==0) printf("prima  %d -->   %f  %f  \n", myid ,  A[1*LD+1], A[1*LD+398]);
-  if (myid==3) printf("centro %d -->   %f  %f  \n", myid ,  A[49*LD+199], A[49*LD+200]);
-  if (myid==4) printf("centro %d -->   %f  %f  \n", myid ,  A[00*LD+199], A[00*LD+200]);
-  if (myid==7) printf("ultima %d -->   %f  %f  \n", myid ,  A[48*LD+1], A[48*LD+398]);
+  if (myid == 0)
+    printf("prima  %d -->   %f  %f  \n", myid, A[1 * LD + 1], A[1 * LD + 398]);
+  if (myid == 3)
+    printf("centro %d -->   %f  %f  \n", myid, A[49 * LD + 199],
+           A[49 * LD + 200]);
+  if (myid == 4)
+    printf("centro %d -->   %f  %f  \n", myid, A[00 * LD + 199],
+           A[00 * LD + 200]);
+  if (myid == 7)
+    printf("ultima %d -->   %f  %f  \n", myid, A[48 * LD + 1],
+           A[48 * LD + 398]);
 
   MPI_Finalize();
-
 }
